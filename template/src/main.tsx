552dbbbac59d54c8e23a0c293dcfb55d94b413ed
shenjianZ <shenjianZLT@gmail.com>
2026-03-06 12:28:17 +0800

    fix main.ts import

--- template/src/main.tsx
+++ template/src/main.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// import "react-docs-ui/style.css";
-// import { DocsApp } from "react-docs-ui";
-import "../../../react-docs-ui/dist/react-docs-ui.css";
+import "react-docs-ui/style.css";
+import { DocsApp } from "react-docs-ui";
+// import "../../../react-docs-ui/dist/react-docs-ui.css";
 // @ts-ignore using local built ES module for development
-import { DocsApp } from "../../../react-docs-ui/dist/react-docs-ui.es.js";
+// import { DocsApp } from "../../../react-docs-ui/dist/react-docs-ui.es.js";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
