d6f08716c5c00b6f0cd950cf095d6a3809591fdf
shenjianZ <shenjianZLT@gmail.com>
2026-03-27 11:05:06 +0800

    chorne: version v0.6.18 publish

--- template/src/main.tsx
+++ template/src/main.tsx
@@ -4,13 +4,13 @@ window.Buffer = Buffer;
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// import "react-docs-ui/style.css";
-// import { DocsApp, preloadDocsRuntime } from "react-docs-ui/docs-app";
-import "../../../react-docs-ui/dist/react-docs-ui.css";
-import {
-    DocsApp,
-    preloadDocsRuntime,
-} from "../../../react-docs-ui/dist/docs-app.es.js";
+import "react-docs-ui/style.css";
+import { DocsApp, preloadDocsRuntime } from "react-docs-ui/docs-app";
+// import "../../../react-docs-ui/dist/react-docs-ui.css";
+// import {
+// DocsApp,
+// preloadDocsRuntime,
+// } from "../../../react-docs-ui/dist/docs-app.es.js";
 import { siteShikiBundle } from "./generated/shiki-bundle";
 
 preloadDocsRuntime();
